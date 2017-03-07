6f3d2c95c6c60d3ee3dd7a4ea273ac2163ba56e8
Wesley Wei <wesley631w@gmail.com>
2017-03-07 14:51:54 -0800

    Added a server and ui for the shiny app.

---
+++ server.R
@@ -0,0 +1,11 @@
+library(shiny)
+
+ucr.wa.crime.data <- read.csv("data/ucr.wa.crime.data.csv")
+wa.average.crime <- read.csv("data/wa.average.crime.csv")
+
+shinyServer(
+  
+  function(input, output) {
+    
+  }
+)
---
+++ ui.R
@@ -0,0 +1,17 @@
+library(shiny)
+library(ggplot2)
+
+shinyUI(fluidPage(
+  titlePanel(title = "Crime in Washington"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+    )
+  ),
+  
+  mainPanel(h5("Crime in Washington is different in each county.")
+    
+  )
+  
+))
