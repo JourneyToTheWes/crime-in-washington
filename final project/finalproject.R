ede3221b3ea3aba8d4cf45bc781c8e71bd367098
chencal <chencal@uw.edu>
2017-03-06 17:37:09 -0800

    UCR data

--- final project/finalproject.R
+++ final project/finalproject.R
@@ -11,12 +11,15 @@ View(crime.stats)
 basic.crime.stats <- select(crime.stats, year, county, POP_F_TOTAL, POP_M_TOTAL, 13:213)
 #Filters out years from 1990-1999
 filter.years <- basic.crime.stats[!grepl("199", basic.crime.stats$year),]
+View(filter.years)
 #Sum of Population Population - MAY CHECK UP ON THIS
 filter.years <- mutate(filter.years, Total.Pop = POP_F_TOTAL + POP_M_TOTAL)
-#Select total crimes under each category
-crime.category <- select(filter.years, year, county, POP_F_TOTAL, POP_M_TOTAL,UCR_TOTAL,NIB_TOTAL,ARR_TOTAL,ARN_TOTAL,SCF_TOTAL,JST_TOTAL)
-View(crime.category)
-
-
+View(filter.years)
+#Select total crimes under UCR - United Crime Report
+crime.category <- select(filter.years, year, county, POP_F_TOTAL, POP_M_TOTAL,UCR_AG_ASSLT,UCR_ARSON,UCR_BURGLARY,UCR_MURDER,UCR_MVT,UCR_RAPE,UCR_ROBBERY,UCR_THEFT,UCR_TOTAL)
+filter <- mutate(crime.category, TOTAL.POP = POP_F_TOTAL + POP_M_TOTAL)
+#Brings total.pop to front
+crime.pop.front<- select(filter, year, county, POP_F_TOTAL, POP_M_TOTAL, TOTAL.POP,UCR_AG_ASSLT,UCR_ARSON,UCR_BURGLARY,UCR_MURDER,UCR_MVT,UCR_RAPE,UCR_ROBBERY,UCR_THEFT,UCR_TOTAL)
+View(crime.pop.front)
 
 
