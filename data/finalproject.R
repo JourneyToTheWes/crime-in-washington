d1ca2b98386f566e91c310d20c6becba524d2ece
chencal <chencal@uw.edu>
2017-03-08 20:47:57 -0800

    Conflict with boxplot

--- data/finalproject.R
+++ data/finalproject.R
@@ -13,15 +13,16 @@ basic.crime.stats <- select(crime.stats, year, county, POP_F_TOTAL, POP_M_TOTAL,
 filter.years <- basic.crime.stats[!grepl("199", basic.crime.stats$year),]
 filter.years2012 <- filter.years[!grepl("2012", filter.years$year),]
 filter.years2013 <- filter.years2012[!grepl("2013", filter.years2012$year),]
-new.filter.years <- filter.years2013[!grepl("2014", filter.years2013$year),]
+filter.years.removed <- filter.years2013[!grepl("2014", filter.years2013$year),]
 
 #Sum of Population Population - MAY CHECK UP ON THIS
-new.filter.years <- mutate(filter.years, Total.Pop = POP_F_TOTAL + POP_M_TOTAL)
+new.filter.years <- mutate(filter.years.removed, Total.Pop = POP_F_TOTAL + POP_M_TOTAL)
 #Select total crimes under UCR - United Crime Report
-crime.category <- select(filter.years, year, county, POP_F_TOTAL, POP_M_TOTAL,UCR_AG_ASSLT,UCR_ARSON,UCR_BURGLARY,UCR_MURDER,UCR_MVT,UCR_RAPE,UCR_ROBBERY,UCR_THEFT,UCR_TOTAL)
+crime.category <- select(new.filter.years, year, county, POP_F_TOTAL, POP_M_TOTAL,UCR_AG_ASSLT,UCR_ARSON,UCR_BURGLARY,UCR_MURDER,UCR_MVT,UCR_RAPE,UCR_ROBBERY,UCR_THEFT,UCR_TOTAL)
 filter <- mutate(crime.category, TOTAL.POP = POP_F_TOTAL + POP_M_TOTAL)
 #Brings total.pop to front
 crime.pop.front <- select(filter, year, county, POP_F_TOTAL, POP_M_TOTAL, TOTAL.POP,UCR_AG_ASSLT,UCR_ARSON,UCR_BURGLARY,UCR_MURDER,UCR_MVT,UCR_RAPE,UCR_ROBBERY,UCR_THEFT,UCR_TOTAL)
+View(crime.pop.front)
 write.csv(crime.pop.front, file = "ucr.wa.crime.data.csv")
 
 #Average of each crime committed within the years
@@ -29,6 +30,7 @@ average.crime <- crime.pop.front %>%
   group_by(county) %>% 
   summarize(Average_Population =mean(TOTAL.POP),Average_Assult =mean(UCR_AG_ASSLT), Average_Arson = mean(UCR_ARSON), Average_Burglary= mean(UCR_BURGLARY), Average_Murder= mean(UCR_MURDER),
             Average_MVT =mean(UCR_MVT), Average_Rape =mean(UCR_RAPE), Average_Robbery =mean(UCR_ROBBERY), Average_Theft =mean(UCR_THEFT), Average_Total =mean(UCR_TOTAL))
+View(average.crime)
 write.csv(average.crime, file = "wa.average.crime.csv")
 
 
