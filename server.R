e7f7785e86066662cdf5152ba108e0de48ac7936
Josh Huang <joshht@comcast.net>
2017-03-08 20:56:49 -0800

    add shiny app with interactive scatter plot

--- server.R
+++ server.R
@@ -1,11 +1,37 @@
 library(shiny)
+library(dplyr)
+library(plotly)
 
-ucr.wa.crime.data <- read.csv("data/ucr.wa.crime.data.csv")
 wa.average.crime <- read.csv("data/wa.average.crime.csv")
+wa.average.crime.select <- filter(wa.average.crime, county != 'STATE')
 
-shinyServer(
-  
-  function(input, output) {
+wa.average.select.crime<- filter(wa.average.crime, county != 'STATE') %>% select(county,Average_Population, Average_Assult,
+                                 Average_Arson, Average_Burglary, Average_Murder, Average_MVT, 
+                                 Average_Rape, Average_Robbery, Average_Theft)
+server<- function(input, output){
+
+    output$plot <- renderPlotly({
+      
+      p <- plot_ly(wa.average.crime.select, x= ~Average_Population,y= ~Average_Total)
+      
+      return(p)
+    })
+    
+    output$plot2 <- renderPlotly({
+      name <- input$choice
+      
+      x<- select_(wa.average.select.crime, input$choice)
+      
+      p <- ggplot(data = wa.average.select.crime, mapping = aes(x = select_(wa.average.select.crime, input$choice), y = county)) +
+        geom_point() +
+        xlab('Occurance')
+      
+      p <- ggplotly(p)
+      
+      
+      return(p)
+    })
+    
     
   }
-)
+
--- ui.R
+++ ui.R
@@ -1,16 +1,30 @@
 library(shiny)
 library(ggplot2)
 
-shinyUI(fluidPage(
+crime.total <- range(wa.average.crime$Average_Total)
+pop <- range(wa.average.crime$Average_Population)
+wa.average.select.crime<- select(wa.average.crime.select, Average_Population,Average_Assult,
+                                 Average_Arson, Average_Burglary, Average_Murder, Average_MVT, 
+                                 Average_Rape, Average_Robbery, Average_Theft)
+
+ui <- fluidPage(
   titlePanel(title = "Crime in Washington"),
   
   sidebarLayout(
-    sidebarPanel(
-      h5("hello")
-    ),
     
-    mainPanel(h5("Crime in Washington is different in each county."),
-              
+    sidebarPanel(
+      
+      selectInput('choice', label="Crime", choices= colnames(wa.average.select.crime))
+      
+    ),
+
+    mainPanel(
+      
+      h5("Crime in Washington is different in each county."),
+      
+      plotlyOutput('plot'),
+      
+      plotlyOutput('plot2')
     )
   )
-))
+)
