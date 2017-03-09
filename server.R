964a073322df79e9bc0a480641208c8820f76a08
chencal <chencal@uw.edu>
2017-03-08 20:50:53 -0800

    ocnflicts with boxplot

--- server.R
+++ server.R
@@ -1,11 +1,35 @@
 library(shiny)
 
-ucr.wa.crime.data <- read.csv("data/ucr.wa.crime.data.csv")
-wa.average.crime <- read.csv("data/wa.average.crime.csv")
+ucr.wa.crime.data <- read.csv("ucr.wa.crime.data.csv")
+wa.average.crime <- read.csv("wa.average.crime.csv")
 
-shinyServer(
+server <-function(input, output){
+  filtered <- reactive({
+    data <- ucr.wa.crime.data %>% 
+      filter(county == input$county)
+    return(data)
+  })
   
-  function(input, output) {
-    
-  }
-)
+  output$county <- renderText({
+    filtered()
+  })
+
+  output$table <- renderDataTable({
+    output$mytable <- renderText(input$county)
+    return(filtered())
+  })
+  
+  
+  output$plot <- renderPlot({
+    while(ucr.wa.crime.data$county == input$county){
+      boxplot(as.formula(filtered()), 
+              data = wa.average.crime,
+              outline = input$outliers)
+    }
+  })
+  
+  
+}
+
+shinyApp(ui=shinyUI(ui),server=shinyServer(server))
+
--- ui.R
+++ ui.R
@@ -1,16 +1,35 @@
-library(shiny)
-library(ggplot2)
+library('shiny')
+library('ggplot2')
+library('dplyr')
+library('plotly')
+ucr.wa.crime.data <- read.csv('ucr.wa.crime.data.csv')
+wa.average.crime <- read.csv('wa.average.crime.csv')
 
-shinyUI(fluidPage(
+
+
+
+ui <- fluidPage(
   titlePanel(title = "Crime in Washington"),
   
   sidebarLayout(
     sidebarPanel(
-      h5("hello")
+      h5("Please Choose a County"),
+      selectInput('county', label = 'County', choices  = c("ADAMS", "ASOTIN","BENTON","CHELAN","CLALLAM","CLARK","COLUMBIA",
+                                                           "COWLITZ","DOUGLAS","FERRY","FRANKLIN","GARFIELD","GRANT","GRAYS HARBOR","ISLAND",
+                                                           "JEFFERSON","KING","KITSAP","KITTITAS","KLICKITAT","LEWIS","LINCOLN","MASON",
+                                                           "OKANOGAN","PACIFIC","PEND OREILLE","PIERCE","SAN JUAN","SKAGIT","SKAMANIA",
+                                                           "SNOHOMISH","SPOKANE","STATE","STEVENS","THURSTON","WAHKIAKUM","WALLA WALLA","WHATCOM",
+                                                           "WHITMAN","YAKIMA"), selected = "ADAMS"),
+      checkboxInput("outliers", "Show outliers", FALSE)
+      
     ),
     
-    mainPanel(h5("Crime in Washington is different in each county."),
+    mainPanel(h5("Average Crime In a County. "),
+              dataTableOutput('table'),
+              plotOutput('plot', click = "plot_click"),
+              verbatimTextOutput("info")
               
     )
   )
-))
+)
+
