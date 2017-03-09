2d394fb09964e08858a9f180ab7779088a40bcac
chencal <chencal@uw.edu>
2017-03-09 07:18:16 -0800

    Description

---
+++ Description.R
@@ -0,0 +1,9 @@
+The boxplot below displays the overall statistics of a specific crime given any county within the Washington state.
+This data visualization directly gives Washington residents information about the crimes being frequented in the premises
+in their own county and what they should look out for. The boxplot shows the minimum and max of a specific crime. Also, 
+it provides the median of the crime and shows outliers within the year 2000-2011. 
+
+The Table provides the exact statistics about each county and the crime. It also displays the female,male, and total population 
+within the years ranging from 2000-2011. The sliderinput filters out the interest of counties and provides search bars at the 
+bottom and at the top to filter out more specific elements the user requests. 
+
