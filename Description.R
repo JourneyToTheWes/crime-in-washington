06d1ba773c9f7bd50add10b4ef16617212b650a0
chencal <chencal@uw.edu>
2017-03-09 07:26:26 -0800

    Label which visual is which

--- Description.R
+++ Description.R
@@ -1,18 +1,26 @@
+
+BOXPLOT
 The boxplot below displays the overall statistics of a specific crime given any county within the Washington state.
 This data visualization directly gives Washington residents information about the crimes being frequented in the premises
 in their own county and what they should look out for. The boxplot shows the minimum and max of a specific crime. Also, 
 it provides the median of the crime and shows outliers within the year 2000-2011. 
 
+
+TABLE
 The Table provides the exact statistics about each county and the crime. It also displays the female,male, and total population 
 within the years ranging from 2000-2011. The sliderinput filters out the interest of counties and provides search bars at the 
 bottom and at the top to filter out more specific elements the user requests. 
 
-The scatter plot compares the average population to the total crimes, showing correlation, and the second scatter plot displays 
-the number of occurance of the crime the user chose in each county. 
 
-The boxplot shows the overall statistics of the different crimes 
-being committed in a given county. The map of the state shows the highest crime committed within a certain county.
+SCATTERPLOT
+The scatter plot compares the average population to the total crimes, showing correlation, 
 
+
+SCATTERPLOT
+This scatter plot displays the number of occurance of the crime the user chose in each county. 
+
+
+MAP
 The map gives us a complete visual for the most crimes frequented in each county. Different colors represent different
 crimes most committed in each county. This shows what users could be on the look out in order to prevent these crimes from
 happening. 
