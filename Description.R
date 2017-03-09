0c333d589f542be91e245c38104ea0aed0c43ddf
chencal <chencal@uw.edu>
2017-03-09 07:23:46 -0800

    added more descritions

--- Description.R
+++ Description.R
@@ -7,3 +7,12 @@ The Table provides the exact statistics about each county and the crime. It also
 within the years ranging from 2000-2011. The sliderinput filters out the interest of counties and provides search bars at the 
 bottom and at the top to filter out more specific elements the user requests. 
 
+The scatter plot compares the average population to the total crimes, showing correlation, and the second scatter plot displays 
+the number of occurance of the crime the user chose in each county. 
+
+The boxplot shows the overall statistics of the different crimes 
+being committed in a given county. The map of the state shows the highest crime committed within a certain county.
+
+The map gives us a complete visual for the most crimes frequented in each county. Different colors represent different
+crimes most committed in each county. This shows what users could be on the look out in order to prevent these crimes from
+happening. 
