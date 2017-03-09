202e91f2aaaed408c4f36c73908c83dfedbd20b1
Josh Huang <joshht@comcast.net>
2017-03-09 00:15:29 -0800

    fix plots and add descriptions

--- server.R
+++ server.R
@@ -9,22 +9,31 @@ wa.average.select.crime<- filter(wa.average.crime, county != 'STATE') %>% select
                                  Average_Arson, Average_Burglary, Average_Murder, Average_MVT, 
                                  Average_Rape, Average_Robbery, Average_Theft)
 server<- function(input, output){
-
+    #make a plotly scatter plot for Average Population V. Average Total Crime in order to see correlation 
     output$plot <- renderPlotly({
       
-      p <- plot_ly(wa.average.crime.select, x= ~Average_Population,y= ~Average_Total)
+      p <- plot_ly(wa.average.crime.select, x= ~Average_Population,y= ~Average_Total, size = ~Average_Population, color = ~Average_Population,
+                   type = 'scatter')
       
       return(p)
     })
     
-    output$plot2 <- renderPlotly({
-      name <- input$choice
+    output$plot1 <- renderPlot({
       
-      x<- select_(wa.average.select.crime, input$choice)
+      p <- ggplot(data = wa.average.crime.select, mapping = aes(x = Average_Population, y = Average_Total))+
+        geom_point()
+      
+      p <- p + geom_smooth(se = FALSE)
+      
+      return(p)
+    })
+    
+    #make a plotly scatter plot, and use the user input for different visualization
+    output$plot2 <- renderPlotly({
       
       p <- ggplot(data = wa.average.select.crime, mapping = aes(x = select_(wa.average.select.crime, input$choice), y = county, color = county)) +
         geom_point() +
-        xlab('Occurance')
+        xlab('Crime Occurance')
       
       p <- ggplotly(p)
       
--- ui.R
+++ ui.R
@@ -1,8 +1,6 @@
 library(shiny)
 library(ggplot2)
 
-crime.total <- range(wa.average.crime$Average_Total)
-pop <- range(wa.average.crime$Average_Population)
 wa.average.select.crime<- select(wa.average.crime.select, Average_Population,Average_Assult,
                                  Average_Arson, Average_Burglary, Average_Murder, Average_MVT, 
                                  Average_Rape, Average_Robbery, Average_Theft)
@@ -14,6 +12,7 @@ ui <- fluidPage(
     
     sidebarPanel(
       
+      #make options for users to choose what time of crime data he/she would like to see
       selectInput('choice', label="Crime", choices= colnames(wa.average.select.crime))
       
     ),
@@ -22,9 +21,21 @@ ui <- fluidPage(
       
       h5("Crime in Washington is different in each county."),
       
+      p("The below visualization provides visualization for the number of crime occurances of each county in Washington State.
+        The plot changes depending on user's choice of crime"),
+      plotlyOutput('plot2', width = 1000, height = 800),
+      
+      #output as plotly and formating the graph
+      p("The below visualization shows a plot of average population of each county in Wahsingont State
+        V. average total crime in the county from year",em('2000'), "to year", em("2011"), ". The visualization
+        allows us to see that there's is a", strong("directly proportional"), "correlation between the average population and
+        the average crime. The more population in a county, the higer the crime rate"),
       plotlyOutput('plot', width = 1000, height = 500),
       
-      plotlyOutput('plot2', width = 1200, height = 500)
+      p('The visualization below draws a line to show the correlation'),
+      plotOutput('plot1')
+      
+      
     )
   )
 )
